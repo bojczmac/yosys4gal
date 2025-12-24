6b884f0f0ef270d21b88217e652f789d36601f71
annoyatron255 <annoyatron255@gmail.com>
2025-12-24 17:02:16 -0600

    Fix trivial registered tristate synthesis
    Tristate is still somewhat experimental. Likely more complex tristate
    logic will need some additional work.

--- synth_gal.tcl
+++ synth_gal.tcl
@@ -79,6 +79,9 @@ techmap -map techmaps/olmc_seq.v
 techmap -max_iter 1 -map techmaps/one_sop.v */t:GAL_TRI "%x:+\[E\]" */t:GAL_TRI %d %ci1 */t:GAL_SOP %i
 techmap -max_iter 1 -map techmaps/one_sop.v */t:GAL_TRI_N "%x:+\[E\]" */t:GAL_TRI_N %d %ci1 */t:GAL_SOP %i
 
+# Make 1SOPs for registered OLMC tristates
+techmap -max_iter 1 -map techmaps/one_sop.v */t:GAL_OLMC "%x:+\[E\]" */t:GAL_OLMC %d %ci1 */t:GAL_SOP %i
+
 # Add OLMC for internal GAL_SOPs
 #techmap -max_iter 1 -map techmaps/pla_olmc_int.v */t:GAL_OLMC %ci2 */t:GAL_SOP %i */t:GAL_SOP %D
 techmap -max_iter 1 -map techmaps/pla_olmc_int.v */t:GAL_SOP %co1 */w:* %i */t:GAL_SOP %ci1 */w:* %i %i %c %ci1 %D
